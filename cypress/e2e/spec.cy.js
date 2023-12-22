52f9ac9947b0a4a40593308a8e7569686f5da645
sveta-1970 <ssarisoydan@gmail.com>
2023-12-22 22:52:59 +0200

    Updated test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,54 +6,58 @@ describe("Calculator", () => {
 
   it("should perform addition correctly", () => {
     // Perform addition operation
-    cy.get("#addition-button").click();
     cy.get("#input").type("5");
-    cy.get("#eq").click();
+    cy.get("#addition-button").click({ force: true });
+    cy.get("#input").type("5");
+    cy.get("#eq").click({ force: true });
 
     // Verify the result
-    cy.get("#input").should("have.value", "5");
+    cy.get("#input").should("have.value", "10");
   });
 
   it("should perform subtraction correctly", () => {
     // Perform subtraction operation
-    cy.get("#subtraction-button").click();
+    cy.get("#input").type("5");
+    cy.get("#subtraction-button").click({ force: true });
     cy.get("#input").type("3");
-    cy.get("#eq").click();
+    cy.get("#eq").click({ force: true });
 
     // Verify the result
-    cy.get("#input").should("have.value", "-3");
+    cy.get("#input").should("have.value", "2");
   });
 
   it("should perform division correctly", () => {
     // Perform division operation
-    cy.get("#division-button").click();
+    cy.get("#input").type("20");
+    cy.get("#division-button").click({ force: true });
     cy.get("#input").type("10");
-    cy.get("#eq").click();
+    cy.get("#eq").click({ force: true });
 
     // Verify the result
-    cy.get("#input").should("have.value", "0");
+    cy.get("#input").should("have.value", "2");
   });
 
   it("should perform multiplication correctly", () => {
     // Perform multiplication operation
-    cy.get("#multiplication-button").click();
+    cy.get("#input").type("5");
+    cy.get("#multiplication-button").click({ force: true });
     cy.get("#input").type("7");
-    cy.get("#eq").click();
+    cy.get("#eq").click({ force: true });
 
     // Verify the result
-    cy.get("#input").should("have.value", "0");
+    cy.get("#input").should("have.value", "35");
   });
 
   it("should change positive number to negative and vice versa", () => {
     // Change positive number to negative
     cy.get("#input").type("5");
-    cy.get("#negate-button").click();
+    cy.get("#negate-button").click({ force: true });
 
     // Verify the result
     cy.get("#input").should("have.value", "-5");
 
     // Change negative number to positive
-    cy.get("#negate-button").click();
+    cy.get("#negate-button").click({ force: true });
 
     // Verify the result
     cy.get("#input").should("have.value", "5");
@@ -61,7 +65,7 @@ describe("Calculator", () => {
 
   it("should clear number from display", () => {
     cy.get("#input").type("5");
-    cy.get("#clear-button").click();
+    cy.get("#clear-button").click({ force: true });
 
     // Verify the result
     cy.get("#input").should("have.value", "0");
@@ -69,7 +73,7 @@ describe("Calculator", () => {
 
   it("should take percent from number", () => {
     cy.get("#input").type("5");
-    cy.get("#percent").click();
+    cy.get("#percent").click({ force: true });
 
     // Verify the result
     cy.get("#input").should("have.value", "0.05");
