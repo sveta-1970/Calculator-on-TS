b1ed0acc4d20cbef05ac4cc62ee70a11c0de501b
sveta-1970 <ssarisoydan@gmail.com>
2024-01-21 19:36:33 +0200

    Added tests for memory

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -78,4 +78,72 @@ describe("Calculator", () => {
     // Verify the result
     cy.get("#input").should("have.value", "0.05");
   });
+
+  it("should place number from display to memory, show 0 in display and flag m at the left top", () => {
+    cy.get("#input").type("5");
+    cy.get("#memoryIn").click({ force: true });
+
+    // Verify the result
+    cy.get("#input").should("have.value", "0");
+    cy.get("#memoryFlag").should("have.value", "m");
+  });
+
+  it("should add number in display to number in memory and update display and memory", () => {
+    // Set initial value in memory
+    cy.get("#input").type("5");
+    cy.get("#memoryIn").click({ force: true });
+
+    // Enter another number in display and click memory button again
+    cy.get("#input").type("7");
+    cy.get("#memoryIn").click({ force: true });
+
+    // Verify the result
+    cy.get("#input").should("have.value", "0"); // Display should show "0"
+    cy.get("#memoryFlag").should("have.text", "m"); // Memory flag should remain "m"
+    cy.get("#memoryIn").should("have.value", "12"); // Memory should contain 12 (5 + 7)
+  });
+
+  it("should minus number in display from number in memory and update display and memory", () => {
+    // Set initial value in memory
+    cy.get("#input").type("5");
+    cy.get("#memoryIn").click({ force: true });
+
+    // Enter another number in display and click "M-" button 
+    cy.get("#input").type("3");
+    cy.get("#memoryOut").click({ force: true });
+
+    // Verify the result
+    cy.get("#input").should("have.value", "0"); // Display should show "0"
+    cy.get("#memoryFlag").should("have.text", "m"); // Memory flag should remain "m"
+    cy.get("#memoryIn").should("have.value", "2"); // Memory should contain 2 (5 - 3)
+  });
+
+  it("should show number from memory in display", () => {
+    // Set initial value in memory
+    cy.get("#input").type("5");
+    cy.get("#memoryIn").click({ force: true });
+
+    //Click "MRC" button one time
+    cy.get("#clear-memory").click({ force: true });
+
+    // Verify the result
+    cy.get("#input").should("have.value", "5"); // Display should show "5"
+    cy.get("#memoryFlag").should("have.text", ""); // Memory flag should show ""
+  })
+
+  it("should clear memory", () => {
+    // Set initial value in memory
+    cy.get("#input").type("5");
+    cy.get("#memoryIn").click({ force: true });
+
+    //Click "MRC" button one time
+    cy.get("#clear-memory").click({ force: true });
+
+    //Click "MRC" button second time
+    cy.get("#clear-memory").click({ force: true });
+
+    // Verify the result
+    cy.get("#input").should("have.value", "0"); // Display should show "5"
+    cy.get("#memoryFlag").should("have.text", ""); // Memory flag should show ""
+  })
 });
